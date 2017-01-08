3a8fe78701596c5a59eb68df767bb42b4bef29dc
Marcus Harrison <marcus@harrisonland.co.uk>
2017-01-08 22:41:56 +0000

    Add functions to feed and play with your fluffle
    Complete main loop
    
    Fix reduce function

--- fluffles.c
+++ fluffles.c
@@ -8,9 +8,12 @@ struct fluffle {
                hunger;
 };
 
-double increase(double current);
-double decrease(double current);
-int loop(void);
+double increase(double);
+double decrease(double);
+char *name_loop(FILE *, char, size_t);
+char *get_name(FILE *);
+int loop(char, struct fluffle);
+void clean(struct fluffle);
 
 double increase(double current)
 {
@@ -19,12 +22,68 @@ double increase(double current)
 
 double decrease(double current)
 {
-        return 5 / current;
+        return current / ((current + 1) * 2);
 }
 
-int loop(void)
+struct fluffle play(struct fluffle current)
 {
-        return 0;
+        struct fluffle result = {
+                current.name,
+                current.love,
+                increase(current.happiness),
+                decrease(current.hunger)
+        };
+        return result;
+}
+
+struct fluffle feed(struct fluffle current)
+{
+        struct fluffle result = {
+                current.name,
+                current.love,
+                current.happiness,
+                increase(current.hunger)
+        };
+        return result;
+}
+
+void print_fluffle(struct fluffle fluffle)
+{
+        char *outformat = "%s's feelings:\n"
+                "Love: %f\n"
+                "Happiness: %f\n"
+                "Hunger: %f\n";
+        printf(
+                        outformat,
+                        fluffle.name,
+                        fluffle.love,
+                        fluffle.happiness,
+                        fluffle.hunger
+              );
+}
+
+int loop(char input, struct fluffle players_fluffle)
+{
+        struct fluffle update;
+        switch (input) {
+                case '\n':
+                        return loop(getchar(), players_fluffle);
+                case 'q':
+                        clean(players_fluffle);
+                        return 0;
+                case 'p':
+                        update = play(players_fluffle);
+                        break;
+                case 'f':
+                        update = feed(players_fluffle);
+                        break;
+                case 'c':
+                default:
+                        update = players_fluffle;
+        }
+        print_fluffle(update);
+        printf("Enter a command ([p]lay, [f]eed, [c]heck, [q]uit):");
+        return loop(getchar(), update);
 }
 
 char *name_loop(FILE *stream, char input, size_t size) {
@@ -47,11 +106,19 @@ char *get_name(FILE *file)
         return name_loop(file, getc(file), 0);
 }
 
+void clean(struct fluffle players_fluffle)
+{
+        free(players_fluffle.name);
+}
+
 int main(int argc, char **argv)
 {
-        struct fluffle players_fluffle = { 0 };
+        struct fluffle players_fluffle = {
+                0,
+                hunger: 5.0
+        };
         printf("What's your fluffle's name?\n");
         players_fluffle.name = get_name(stdin);
         printf("Right! Your fluffle's name is %s!\n", players_fluffle.name);
-        return loop();
+        return loop(0, players_fluffle);
 }
